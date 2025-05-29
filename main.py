17674203e7dd590ef22d8a120667c67a43f85148
Gustavo <gustavo.ticona.q@gmail.com>
2025-05-29 18:52:34 -0500

    Actualizar

--- main.py
+++ main.py
@@ -11,26 +11,30 @@ app = Flask(__name__)
 main_html = """
 <html>
 <head></head>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
 <script>
   var mousePressed = false;
   var lastX, lastY;
   var ctx;
 
-   function getRndInteger(min, max) {
+  function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min) ) + min;
-   }
+  }
 
   function InitThis() {
       ctx = document.getElementById('myCanvas').getContext("2d");
 
-      // Por ahora usamos una letra fija
-      var letras = ["alpha", "beta", "gamma", "delta", "epsilon", "zeta", "eta", "theta", "iota", "kappa", "lambda", "mu", "nu", "xi", "omicron", "pi", "rho", "sigma", "tau", "upsilon", "phi", "chi", "psi", "omega"];
+      let letras = [
+        "alpha", "beta", "gamma", "delta", "epsilon", "zeta", "eta", "theta",
+        "iota", "kappa", "lambda", "mu", "nu", "xi", "omicron", "pi", "rho",
+        "sigma", "tau", "upsilon", "phi", "chi", "psi", "omega"
+      ];
 
-      var randomIndex = Math.floor(Math.random() * letras.length);
-      var letraSeleccionada = letras[randomIndex];
+      let random = Math.floor(Math.random() * letras.length);
+      let aleatorio = letras[random];
 
-      document.getElementById('mensaje').innerHTML = 'Dibuja la letra griega: ' + letraSeleccionada;
-      document.getElementById('numero').value = letraSeleccionada;
+      document.getElementById('mensaje').innerHTML  = 'Dibujando un ' + aleatorio;
+      document.getElementById('numero').value = aleatorio;
 
       $('#myCanvas').mousedown(function (e) {
           mousePressed = true;
@@ -46,7 +50,8 @@ main_html = """
       $('#myCanvas').mouseup(function (e) {
           mousePressed = false;
       });
-  	  $('#myCanvas').mouseleave(function (e) {
+
+      $('#myCanvas').mouseleave(function (e) {
           mousePressed = false;
       });
   }
@@ -71,49 +76,26 @@ main_html = """
   }
 
   function prepareImg() {
-    var canvas = document.getElementById('myCanvas');
-
-    // Crear un canvas temporal
-    var tempCanvas = document.createElement('canvas');
-    var tempCtx = tempCanvas.getContext('2d');
-    tempCanvas.width = canvas.width;
-    tempCanvas.height = canvas.height;
-
-    // Pintar fondo blanco
-    tempCtx.fillStyle = 'white';
-    tempCtx.fillRect(0, 0, tempCanvas.width, tempCanvas.height);
-
-    // Dibujar el contenido del canvas original encima
-    tempCtx.drawImage(canvas, 0, 0);
-
-    // Ahora convertir a base64 la imagen con fondo blanco
-    document.getElementById('myImage').value = tempCanvas.toDataURL();
-}
-
+     var canvas = document.getElementById('myCanvas');
+     document.getElementById('myImage').value = canvas.toDataURL();
+  }
 </script>
 <body onload="InitThis();">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
-
     <div align="left">
       <img src="https://upload.wikimedia.org/wikipedia/commons/f/f7/Uni-logo_transparente_granate.png" width="300"/>
     </div>
-
-
     <div align="center">
-        <div>
-            <img src="https://www.shutterstock.com/image-vector/greek-alphabet-letters-education-science-600nw-2444755451.jpg" width="400" alt="Guía del alfabeto griego"/>
-        </div>
-        <h1 id="mensaje">Dibujando una letra griega</h1>
+        <h1 id="mensaje">Dibujando...</h1>
         <canvas id="myCanvas" width="200" height="200" style="border:2px solid black"></canvas>
-        <br/><br/>
+        <br/>
+        <br/>
         <button onclick="javascript:clearArea();return false;">Borrar</button>
     </div>
-
     <div align="center">
-      <form method="post" action="upload" onsubmit="javascript:prepareImg();" enctype="multipart/form-data">
-        <input id="numero" name="numero" type="hidden" value="">
-        <input id="myImage" name="myImage" type="hidden" value="">
-        <input id="bt_upload" type="submit" value="Enviar">
+      <form method="post" action="upload" onsubmit="javascript:prepareImg();"  enctype="multipart/form-data">
+      <input id="numero" name="numero" type="hidden" value="">
+      <input id="myImage" name="myImage" type="hidden" value="">
+      <input id="bt_upload" type="submit" value="Enviar">
       </form>
     </div>
 </body>
@@ -122,43 +104,51 @@ main_html = """
 
 @app.route("/")
 def main():
-    return(main_html)
+    return main_html
 
 @app.route('/upload', methods=['POST'])
 def upload():
     try:
         img_data = request.form.get('myImage').replace("data:image/png;base64,","")
-        letra = request.form.get('numero')
-        print("Letra:", letra)
-
-        if not os.path.exists(letra):
-            os.mkdir(letra)
-
-        with tempfile.NamedTemporaryFile(delete=False, mode="w+b", suffix='.png', dir=letra) as fh:
+        aleatorio = request.form.get('numero')
+        print(f"Leyendo imagen para clase: {aleatorio}")
+        if not os.path.exists(str(aleatorio)):
+            os.mkdir(str(aleatorio))
+        with tempfile.NamedTemporaryFile(delete=False, mode="w+b", suffix='.png', dir=str(aleatorio)) as fh:
             fh.write(base64.b64decode(img_data))
-
-        print("Imagen guardada")
+        print("Imagen subida correctamente")
     except Exception as err:
-        print("Error:", err)
-
+        print("Error al subir la imagen")
+        print(err)
     return redirect("/", code=302)
 
 @app.route('/prepare', methods=['GET'])
 def prepare_dataset():
-    letras = ["alpha", "beta", "gamma", "delta", "epsilon", "zeta", "eta", "theta", "iota", "kappa", "lambda", "mu", "nu", "xi", "omicron", "pi", "rho", "sigma", "tau", "upsilon", "phi", "chi", "psi", "omega"]
     images = []
     labels = []
+    letras = [
+        "alpha", "beta", "gamma", "delta", "epsilon", "zeta", "eta", "theta",
+        "iota", "kappa", "lambda", "mu", "nu", "xi", "omicron", "pi", "rho",
+        "sigma", "tau", "upsilon", "phi", "chi", "psi", "omega"
+    ]
     for letra in letras:
         filelist = glob.glob(f'{letra}/*.png')
-        imgs = io.concatenate_images(io.imread_collection(filelist))
-        imgs = imgs[:, :, :, 3]  # Alpha channel
-        images.append(imgs)
-        labels.append(np.array([letra] * imgs.shape[0]))
-    images = np.vstack(images)
-    labels = np.concatenate(labels)
-    np.save('X.npy', images)
-    np.save('y.npy', labels)
-    return "OK!"
+        if len(filelist) == 0:
+            print(f"No se encontraron imágenes para la clase {letra}")
+            continue
+        images_read = io.concatenate_images(io.imread_collection(filelist))
+        images_read = images_read[:, :, :, 3]
+        labels_for_class = np.array([letra] * images_read.shape[0])
+        images.append(images_read)
+        labels.append(labels_for_class)
+    if images:
+        images = np.vstack(images)
+        labels = np.concatenate(labels)
+        np.save('X.npy', images)
+        np.save('y.npy', labels)
+        return "Dataset preparado OK!"
+    else:
+        return "No hay imágenes para preparar dataset."
 
 @app.route('/X.npy', methods=['GET'])
 def download_X():
@@ -169,6 +159,12 @@ def download_y():
     return send_file('./y.npy')
 
 if __name__ == "__main__":
-    if not os.path.exists('alpha'):
-        os.mkdir('alpha')
+    letras = [
+        "alpha", "beta", "gamma", "delta", "epsilon", "zeta", "eta", "theta",
+        "iota", "kappa", "lambda", "mu", "nu", "xi", "omicron", "pi", "rho",
+        "sigma", "tau", "upsilon", "phi", "chi", "psi", "omega"
+    ]
+    for letra in letras:
+        if not os.path.exists(str(letra)):
+            os.mkdir(str(letra))
     app.run()
